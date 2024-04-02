02c19e46f1671fee0dbdafad1f5162f42c897048
nischay-chauhan <masternischay@outlook.com>
2024-04-02 10:20:17 +0530

    Page-Replacement Algorithm

---
+++ Page-replacement/a.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void FIFOPageReplacement(vector<int>& pages, int capacity) {
+    queue<int> pageQueue;
+    int pageFaults = 0;
+
+    for (int page : pages) {
+        bool found = false;
+        queue<int> tempQueue = pageQueue;
+
+        while (!tempQueue.empty()) {
+            if (tempQueue.front() == page) {
+                found = true;
+                break;
+            }
+            tempQueue.pop();
+        }
+        if (!found) {
+            if (pageQueue.size() == capacity) {
+                pageQueue.pop();
+            }
+            pageQueue.push(page);
+            pageFaults++;
+        }
+    }
+
+    cout << "Total Page Faults: " << pageFaults << endl;
+}
+
+int main() {
+    vector<int> pages = {7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 1, 2, 0};
+    int capacity = 3;
+    FIFOPageReplacement(pages, capacity);
+
+    return 0;
+}
